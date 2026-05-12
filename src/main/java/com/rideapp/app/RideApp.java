8984d7002b8eab164866077f66185659fe5fc981
KevinEBM <kebuitrago_506@unihumboldt.edu.co>
2026-05-12 10:09:51 -0500

    Implementa Singleton e integracion inicial

--- src/main/java/com/rideapp/app/RideApp.java
+++ src/main/java/com/rideapp/app/RideApp.java
@@ -1,1 +1,24 @@
+package com.rideapp.app;
 
+public class RideApp {
+
+    private static RideApp instancia;
+
+    private RideApp() {
+    }
+
+    public static RideApp getInstance() {
+        if (instancia == null) {
+            instancia = new RideApp();
+        }
+        return instancia;
+    }
+
+    public void recibirSolicitud() {
+        System.out.println("[RideApp] Solicitud recibida");
+    }
+
+    public void mostrarInstanciaUnica() {
+        System.out.println("[RideApp] Instancia central activa");
+    }
+}
